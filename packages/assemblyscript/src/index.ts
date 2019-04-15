92252b630ae095cd7281929fcde6d4f8ed6669f3
Willem Wyndham <willemneal@users.noreply.github.com>
2019-04-15 12:27:08 -0400

    Wasmos/kernel (#22)
    * Added First test for Arg passing for wasi
    
    * Fix for each
    
    * added Travis
    
    * Tests pass
    
    Moved wasa to kernel to remove dependency.
    
    * Fixed module export
    
    * Last dependency issue
    
    * fixes #17

--- packages/assemblyscript/src/index.ts
+++ packages/assemblyscript/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./compiler";
-import * as loader from "assemblyscript/lib/loader";
+import * as loader from "assemblyscript/lib/loader/lib";
 export { loader };
 export * from "assemblyscript/lib/host/lib";
 
