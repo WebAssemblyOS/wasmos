6f5d3cacd58b2fcb2862940605459fdac8edcc6f
Willem Wyndham <willem.neal@gmail.com>
2019-04-23 11:46:28 -0400

    Fix build and added as-pect to travis

--- packages/assemblyscript/src/index.ts
+++ packages/assemblyscript/src/index.ts
@@ -2,5 +2,6 @@ export * from "./compiler/compiler";
 import * as loader from "assemblyscript/lib/loader/lib";
 export { loader };
 export * from "assemblyscript/lib/host/lib";
+export * from "./linker";
 
 export async function main() {}
