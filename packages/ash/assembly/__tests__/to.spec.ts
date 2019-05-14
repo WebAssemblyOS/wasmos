a24847064cee1ae856d72948411e858f9d113732
Sai Arvind <sai.arvindg@gmail.com>
2019-05-14 01:31:44 -0400

    more to additions (WebAssemblyOS/wasmos#60)

--- packages/ash/assembly/__tests__/to.spec.ts
+++ packages/ash/assembly/__tests__/to.spec.ts
@@ -1,42 +1,53 @@
 
 import { Console, fs, Process, CommandLine } from '../../../assemblyscript/assembly/wasa/mock';
 import { FileDescriptor } from '../../../assemblyscript/assembly/wasa/mock/fs';
-import { openStdout, Hello, World } from './mocks';
+import { openStdout, Hello, World, readString } from './fixtures';
 import { main as to } from "../bin/to";
 
 type fd = usize;
 
 
 
-var stdout2: FileDescriptor;
+// var stdout2: FileDescriptor;
 // let stdout = Console.stdout;
 
+const testtxt1: string = "testtxt1";
+const testtxt2: string = "This is some text.";
+
+const testfile1: string = "./testfile1.txt";
+const testfile2: string = "./testfile2.txt";
 
 describe("to", (): void => {
   beforeAll(
     (): void => {
-      stdout2 = openStdout();
+      // stdout2 = openStdout();
     }
   );
 
   beforeEach((): void => {
     Console.stdout.reset();
-    stdout2.reset();
+    // stdout2.reset();
     Console.stdout.erase()
+    // fs.delete(testfile1);
+    // fs.delete(testfile2);
     CommandLine.reset();
-    CommandLine.push("echo");
+    CommandLine.push("to");
   })
 
-  it("should print newline by default", (): void => {
-    CommandLine.push(Hello)
-    CommandLine.push(World)
-    to();
-    let str = Hello + " " + World + "\n";
-    let stdoutStr = stdout2.readString()
-    expect<u32>(Console.stdout.tell()).toBe(str.lengthUTF8, "Two extra characters for space and \\n")
-    Console.stdout.reset();
-    expect<string>(Console.stdout.readString()).toBe(Hello + " " + World + "\n")
-    Console.stdout.reset();
-    expect<string>(Console.stdout.readString()).toBe(stdoutStr);
+  it("should print testtxt1", (): void => {
+    CommandLine.push(testtxt1)
+    CommandLine.push(testfile1)
+    to(CommandLine.all());
+    // let stdoutStr: string = stdout2.readString().result
+    // let stdoutStr: string = Console.stdout.readString().result;
+    // let stderrStr: string = Console.stderr.readString().result;
+    // let stdoutStr: string = readString(Console.stdout);
+    // let stderrStr: string = readString(Console.stderr);
+    let stdoutStr: string = fs.readString(Console.stdout.fd).result;
+    let stderrStr: string = fs.readString(Console.stderr.fd).result;
+    log<string>("stdoutStr: " + stdoutStr);
+    log<string>("stderrStr: " + stderrStr);
+    expect<string>(stderrStr).toBe("");
+    expect<string>(stdoutStr).toBe(testtxt1);
   })
 })
--- packages/ash/assembly/bin/to.ts
+++ packages/ash/assembly/bin/to.ts
@@ -1,44 +1,66 @@
-import { getPath } from "../../../assemblyscript/assembly/wasa/mock/path";
+import { WasiResult } from "../../../assemblyscript/assembly/wasa";
+import { Wasi } from '../../../assemblyscript/assembly/wasi';
 
 
 function validFile(fileName: string): bool {
-  let filePath: string = getPath(fileName);
-  let openFile: WasiResult<FileDescriptor> = fs.openForWrite(filePath);
+  log<string>("in validFile function");
+  let openFile: WasiResult<FileDescriptor> = fs.openFile(fileName);
+  log<Wasi.errno>(openFile.error);
   if (!openFile.failed) {
+    log<string>("in validFile function - can open file");
     fs.close(openFile.result.fd);
     return true;
   }
+  log<string>("in validFile function - cannot open file");
   return false;
 }
 
 function writeToFile(txt: string, fileName: string): bool {
-  let filePath: string = getPath(fileName);
-  let openFile: WasiResult<FileDescriptor> = fs.openForWrite(filePath);
+  log<string>("in writeToFile function");
+  let openFile: WasiResult<FileDescriptor> = fs.openFile(fileName);
   if (openFile.failed) {
+    log<string>("in validFile function - cannot open file for write");
     return false;
   } else {
-    let openFileFd: number = openFile.result.fd;
-    let writeToFile: Wasi.errno = fs.writeString(openFileFd, txt);
+    let writeToFile = fs.writeString(openFile.result.fd, txt);
     if (writeToFile == Wasi.errno.SUCCESS) {
-      fs.close(openFileFd);
+      fs.close(openFile.result.fd);
       return true;
     }
   }
   return false;
 }
 
-export function main(txt: string, fileName: string): void {
-  if (!fileName || fileName.length < 1) {
-    Console.error("Wrong arguments. Usage: to <string> <file name>");
-  } else {
+function validToCmd(args: string[]): bool {
+  log<string>("in validToCommand function");
+  if (args.length > 2) {
+    if (args[1].length > 1) {
+      if (args[2].length > 1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export function main(args: string[]): void {
+  log<string>("in to command");
+  if (validToCmd(args)) {
+    log<string>("valid command");
+    let txt: string = args[1];
+    let fileName: string = args[2];
     if (validFile(fileName)) {
+      log<string>("valid file");
       if (writeToFile(txt, fileName)) {
-        Console.write(txt);
+        log<string>("wrote to file");
+        Console.log(txt);
       } else {
         Console.error("Could not write to file: " + fileName);
       }
     } else {
       Console.error("No such file: " + fileName);
     }
+  } else {
+    Console.error("Wrong arguments. Usage: to <text> <file name>");
   }
 }
