655f4d789afbaebf68a68737c215dd6e7a9ca8c0
sarit <seisen10@gmail.com>
2019-05-15 16:09:57 -0400

    passed tests

--- packages/ash/assembly/__tests__/which.spec.ts
+++ packages/ash/assembly/__tests__/which.spec.ts
@@ -1,39 +1,36 @@
+import { main as which } from "../bin/which";
+import { stdout, readString } from "./fixtures"
 
-import { Environ, Console, fs, Process, CommandLine } from '../../../assemblyscript/assembly/wasa/mock';
-import { FileDescriptor } from '../../../assemblyscript/assembly/wasa/mock/fs';
-import { openStdout, Hello, World } from './mocks';
-import { main as echo } from "../bin/echo";
-
-type fd = usize;
-
-
-let env: Environ;
-var stdout2: FileDescriptor;
-// let stdout = Console.stdout;
-
+let path: string;
 
 describe("echo", (): void => {
     beforeAll(
         (): void => {
-            stdout2 = openStdout();
-            env = new Environ();
-            env.add("$PATH", "/test")
+            path = "/home/bob/code";
+            Environ.add("$PATH", path);
         }
     );
 
+    it("should print $PATH environment variable", () => {
+        CommandLine.push("README.md");
+        log<string>(Environ.get("$PATH"));
+        which(CommandLine.all())
+        expect<string>(readString(stdout)).toBe(path + "/" + "README.md" + "\n")
+    });
+
     beforeEach((): void => {
+        stdout.reset();
         Console.stdout.reset();
         Console.stdout.erase();
-        fs.openForWrite("/test/foo");
         CommandLine.reset();
         CommandLine.push("which");
     })
 
-    it("should find a file in my path", (): void => {
-        CommandLine.push("foo")
-        echo(CommandLine.all())
-        Console.stdout.reset();
-        expect<string>(Console.stdout.readString()).toBe("/test/foo")
-    })
+    // it("should find a file in my path", (): void => {
+    //     CommandLine.push("foo")
+    //     which(CommandLine.all())
+    //     Console.stdout.reset();
+    //     expect<string>(Console.stdout.readString().result).toBe("/test/foo")
+    // })
 })
 
--- packages/ash/assembly/bin/which.ts
+++ packages/ash/assembly/bin/which.ts
@@ -1,4 +1,20 @@
+import { join } from "../../../assemblyscript/assembly/wasa/mock/path";
+
 export function main(args: string[]): void {
     // args[1] is the file I'm looking for
     // console.log(full path) if it exists, nothing if it doesn't find it 
+    let paths = Environ.get("$PATH").split(":");
+    let filename = args[1];
+    while (paths.length > 0) {
+        let folder = paths.pop();
+        let path = join([folder, filename]);
+        let result = fs.openFile(path);
+        log<string>(path);
+        if (result.failed) {
+            continue;
+        } else {
+            Console.log(path);
+            break;
+        }
+    }
 }
