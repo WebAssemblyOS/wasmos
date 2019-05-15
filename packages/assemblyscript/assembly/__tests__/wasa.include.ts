7a77163b23697d9b5b70455c04accc34a7108b59
Willem Wyndham <willem.neal@gmail.com>
2019-05-15 10:06:28 -0400

    Added WasiResult to globals in tests

--- packages/assemblyscript/assembly/__tests__/wasa.include.ts
+++ packages/assemblyscript/assembly/__tests__/wasa.include.ts
@@ -1,6 +1,7 @@
 
 import { Console, fs, Process, CommandLine, fd, FileDescriptor } from '../wasa/mock/index';
 import { Wasi } from "../wasi";
+import { WasiResult } from '../wasa/index';
 
 /** This file is included with tests so that the default globals are set up properly. */
 beforeAll(() => {
