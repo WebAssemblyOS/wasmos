0b876add1aea8082ed02cde08b019eecec282f37
Michael Williams <mjwilli8@umd.edu>
2019-05-16 16:02:24 -0400

    wrote tests for cp

--- packages/ash/assembly/__tests__/cp.spec.ts
+++ packages/ash/assembly/__tests__/cp.spec.ts
@@ -1,4 +1,4 @@
-import { stdout, Hello, World, readString, stderr } from './fixtures';
+import { stdout, Hello, World, readString, stderr, openFile } from './fixtures';
 import { main as cp } from "../bin/cp";
 
 describe("cp", (): void => {
@@ -6,6 +6,8 @@ describe("cp", (): void => {
   beforeEach((): void => {
     stdout.reset();
     Console.stdout.erase();
+    stderr.reset();
+    Console.stderr.erase();
     CommandLine.reset();
     CommandLine.push("cp");
   })
@@ -26,18 +28,35 @@ describe("cp", (): void => {
   });
 
   it("should copy source file to destination file", (): void => {
-
+    CommandLine.push("/test");
+    CommandLine.push("/numbers");
+    cp(CommandLine.all());
+    expect<string>(openFile("/numbers").readString().result).toStrictEqual("Hello World");
   });
 
-  it("should copy source file to destination file even if destination file isn't found", (): void => {
-
+  it("should create new file and copy if destination file isn't found", (): void => {
+    CommandLine.push("/test");
+    CommandLine.push("/newfile");
+    cp(CommandLine.all());
+    expect<string>(openFile("/newfile").readString().result).toStrictEqual("Hello World");
   });
 
   it("should copy source file to destination directory", () => {
-
+    CommandLine.push("/test");
+    CommandLine.push("/home");
+    cp(CommandLine.all());
+    expect<bool>(fs.openFile("/home/test").failed).toStrictEqual(false);
+    expect<string>(openFile("/home/test").readString().result).toStrictEqual("Hello World");
   });
 
   it("should copy multiple source files to destination directory", () => {
-
+    CommandLine.push("/test");
+    CommandLine.push("/numbers");
+    CommandLine.push("/home");
+    cp(CommandLine.all());
+    expect<bool>(fs.openFile("/home/test").failed).toStrictEqual(false);
+    expect<bool>(fs.openFile("/home/numbers").failed).toStrictEqual(false);
+    expect<string>(openFile("/home/test").readString().result).toStrictEqual("Hello World");
+    expect<string>(openFile("/home/numbers").readString().result).toStrictEqual("0\\n1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n10\\n11\\n12\\n13\\n14\\n15\\n16\\n17\\n18\\n19");
   });
 })
