2ad29e65e845d8b488c43cf3e1de13581e83e753
Yoni Rawson <yrawson@terpmail.umd.edu>
2019-05-16 12:09:22 -0400

    passing second test

--- packages/ash/assembly/bin/nl.ts
+++ packages/ash/assembly/bin/nl.ts
@@ -2,18 +2,19 @@ export function main(args: string[]): void {
     let res = fs.openFile(args[1]);
     if (res.error) {
         //This is just the log for testing need to use Console.error
-        log<string>(`head: file: No such file or directory`);
+        Console.error("nl: " + args[1] + ": No such file or directory");
+        // log<string>(`head: file: No such file or directory`);
         return;
     }
     let file = res.result;
     let line = file.readLine();
-    var num = 1;
+    let num = 1;
+    Console.stdin.reset();
 
     if (line.failed) {
         return;
     }
     while (!line.failed) {
-        log<string>(line.result + "-")
         //@ts-ignore  Integer does have to string method.
         let intro: string = "    " + num.toString() + "  ";
 
