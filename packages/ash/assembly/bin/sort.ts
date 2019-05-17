9be99616b8917e579cedd59dad637da484a881e7
Muzammil Quadir <mquadir@terpmail.umd.edu>
2019-05-17 11:48:16 -0400

    Sort Added

--- packages/ash/assembly/bin/sort.ts
+++ packages/ash/assembly/bin/sort.ts
@@ -1,16 +1,50 @@
+function stringcomp(s1: string, s2: string): i32 {
+  if (s1 > s2) {
+    return 1;
+  } else if (s1 < s2) {
+    return -1
+  }
 
+  return 0;
+}
 
 
 export function main(args: string[]): void {
 
+  if (args.length > 1) {
 
-  if (args[1] == "-n") {
-    let _args = args.slice(2).join(" ");
-    Console.write(_args, false)
-  } else {
-    let _args = args.slice(1).join(" ")
-    Console.log(_args);
+    let arr: string[] = [];
+
+    for (let i: i32 = 1; i < args.length; i++) {
+
+      let fd = fs.openFile(args[i]);
+
+      if (!fd.failed) {
+
+        let line = fd.result.readLine();
+        while (!line.failed) {
+          arr.push(line.result);
+          line = fd.result.readLine();
+        }
+
+      } else {
+        Console.error("sort: " + args[i] + ": No such file or directory");
+      }
+
+
+
+    }
+
+    let sorted = arr.sort(stringcomp);
+
+    for (let j: i32 = 0; j < sorted.length; j++) {
+      // log<string>(sorted[j]);
+      Console.write(sorted[j]);
+
+    }
+
+
+
   }
-
-
 }
+
