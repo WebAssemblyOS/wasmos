21f97a611279454de0c0f89a6b10f0f770335d6a
Michael Williams <mjwilli8@umd.edu>
2019-05-17 14:42:30 -0400

    more progress on implementing cp

--- packages/ash/assembly/__tests__/cp.spec.ts
+++ packages/ash/assembly/__tests__/cp.spec.ts
@@ -21,17 +21,10 @@ describe("cp", (): void => {
 
   it("should write to stderr if source file not found", (): void => {
     CommandLine.push("/doesnotexist");
-    CommandLine.push("/test");
+    CommandLine.push("/home");
     cp(CommandLine.all())
     expect<usize>(Console.stderr.tell()).toBeGreaterThan(0);
     expect<string>(stderr.readString().result).toStrictEqual("cp: /doesnotexist: No such file or directory\n")
-  });
-
-  it("should copy source file to destination file", (): void => {
-    CommandLine.push("/test");
-    CommandLine.push("/numbers");
-    cp(CommandLine.all());
-    expect<string>(openFile("/numbers").readString().result).toStrictEqual("Hello World");
   });
 
   it("should create new file and copy if destination file isn't found", (): void => {
@@ -41,11 +34,18 @@ describe("cp", (): void => {
     expect<string>(openFile("/newfile").readString().result).toStrictEqual("Hello World");
   });
 
+  it("should copy source file to destination file", (): void => {
+    CommandLine.push("/test");
+    CommandLine.push("/numbers");
+    cp(CommandLine.all());
+    expect<string>(openFile("/numbers").readString().result).toStrictEqual("Hello World");
+  });
+
   it("should copy source file to destination directory", () => {
     CommandLine.push("/test");
     CommandLine.push("/home");
     cp(CommandLine.all());
-    expect<bool>(fs.openFile("/home/test").failed).toStrictEqual(false);
+    expect<bool>(fs.openFile("/home/test").failed).toBeFalsy();
     expect<string>(openFile("/home/test").readString().result).toStrictEqual("Hello World");
   });
 
@@ -54,8 +54,8 @@ describe("cp", (): void => {
     CommandLine.push("/numbers");
     CommandLine.push("/home");
     cp(CommandLine.all());
-    expect<bool>(fs.openFile("/home/test").failed).toStrictEqual(false);
-    expect<bool>(fs.openFile("/home/numbers").failed).toStrictEqual(false);
+    expect<bool>(fs.openFile("/home/test").failed).toBeFalsy();
+    expect<bool>(fs.openFile("/home/numbers").failed).toBeFalsy();
     expect<string>(openFile("/home/test").readString().result).toStrictEqual("Hello World");
     expect<string>(openFile("/home/numbers").readString().result).toStrictEqual("0\\n1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n10\\n11\\n12\\n13\\n14\\n15\\n16\\n17\\n18\\n19");
   });
--- packages/ash/assembly/bin/cp.ts
+++ packages/ash/assembly/bin/cp.ts
@@ -1,11 +1,34 @@
 export function main(args: string[]): void {
   if (args.length > 2) {
-    for (let i: i32 = 1; i < args.length; i++) {
-        let file = fs.openFile(args[i]);
-        if (file.failed) {
-            Console.error("cp: " + args[i] + ": No such file or directory");
-            continue;
+    let dir = fs.openDirectory(args[args.length - 1]);
+    if (!dir.failed) {
+      for (let i: i32 = 1; i < args.length - 1; i++) {
+          let file = fs.openFile(args[i]);
+          if (file.failed) {
+              Console.error("cp: " + args[i] + ": No such file or directory");
+              continue;
+          }
+          fs.createFileAt(dir.result.fd, args[i]);
+      }
+    } else {
+      if (args.length > 3) {
+        Console.error("usage: cp source_file target_file\ncp source_file ... target_directory");
+      } else {
+        let srcFile = fs.openFile(args[1]);
+        if (srcFile.failed) {
+            Console.error("cp: " + args[1] + ": No such file or directory");
         }
+        let dstFile = fs.openFile(args[2]);
+        if (!dstFile.failed) {
+          // File exists, we need to overwrite its contents
+          // One way is to erase/delete it, then recreate
+          // Erase and delete currently don't work well for me
+          //dstFile.result.erase();
+        }
+        fs.createFile(args[2]);
+        dstFile = fs.openFile(args[2]);
+        fs.writeString(dstFile.result.fd, fs.readString(srcFile.result.fd).result);
+      }
     }
   } else {
     Console.error("usage: cp source_file target_file\ncp source_file ... target_directory");
