6517b0a230c14fa6675209f5ee0d030f6209cfd5
John Murray <5672686+JRMurr@users.noreply.github.com>
2019-05-18 14:35:44 -0400

    init

---
+++ packages/ash/assembly/__tests__/fold.spec.ts
@@ -0,0 +1,44 @@
+import { main as fold } from "../bin/fold";
+import { stdout, Hello, World, Hello_World, stderr } from './fixtures';
+
+describe("fold", (): void => {
+
+    beforeEach((): void => {
+        stdout.reset()
+        Console.stdout.erase() //Erases and resets
+        stderr.reset();
+        Console.stderr.erase() //Erases and resets
+        CommandLine.reset();
+        CommandLine.push("fold");
+    });
+
+    it("pass width length", (): void => {
+        CommandLine.push("-w");
+        CommandLine.push("5");
+        CommandLine.push("/test");
+        fold(CommandLine.all())
+        fs.reset(Console.stdout.fd)
+        let output = fs.readString(Console.stdout.fd).result.split('\n');
+        expect<number>(output.length).toBe(4);
+        expect<string>(output[0]).toBe(Hello);
+        expect<string>(output[1]).toBe(" Worl");
+        expect<string>(output[2]).toBe("d");
+        expect<string>(output[3]).toBe("");
+        Console.stdout.reset()
+        expect<string>(Console.stdout.readString().result).toBe(stdout.readString().result);
+    });
+
+    it("use default length", (): void => {
+        CommandLine.push("/test");
+        fold(CommandLine.all())
+        fs.reset(Console.stdout.fd)
+        // let output = fs.readString(Console.stdout.fd).result.split('\n');
+        log<string>(fs.readString(Console.stdout.fd).result);
+        // expect<number>(output.length).toBe(2);
+        // expect<string>(output[0]).toBe(Hello_World);
+        // expect<string>(output[1]).toBe("");
+        // Console.stdout.reset()
+        // expect<string>(Console.stdout.readString().result).toBe(stdout.readString().result);
+    });
+
+})
---
+++ packages/ash/assembly/bin/fold.ts
@@ -0,0 +1,29 @@
+
+
+export function main(args: string[]): void {
+    if (args.length <= 1) {
+        return;
+    }
+    let width = 80;
+    let fileName = args[1];
+    if (args.length == 4) {
+        // assuming args = ['-w', a num, filename]
+        width = parseInt(args[2]) as i32;
+        fileName = args[3];
+    }
+    let file = fs.openFile(fileName);
+    if (file.failed) {
+        Console.error("fold: " + fileName + ": No such file or directory");
+    }
+    let data = file.result.readString().result;
+    let lines = data.split('\n');
+    let newLines: string[] = [];
+    for (let i = 0; i < lines.length; i++) {
+        let arr = lines[i].split('');
+        while (arr.length > 0) {
+            newLines.push(arr.splice(0, width).join(''));
+        }
+    }
+    // log<string>(newLines.join('\n'));
+    Console.log(newLines.join('\n'));
+}
