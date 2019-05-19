c07f3f29f5273994ac50605b9e6c5abec5271513
Fiori Gebrekidan <fiorigebrekidan@gmail.com>
2019-05-19 17:09:55 -0400

    some code

---
+++ packages/ash/assembly/bin/cd.ts
@@ -0,0 +1,18 @@
+import { os } from "../../../../packages/os";
+
+export function main(args: string[]): void {
+  let dir = args[1]; //or fs.getDir(file);
+  if(!dir) {
+    //os.homedir
+  }
+
+/*
+Notes: path_open opens directory
+let plat = platform();
+let fee = fs.getDir;
+let env = Environ.get("HOME");
+let dirname = args[1];
+env = Environ.get("$PATH");
+let home = env.get("HOME")!;
+  */
+}
