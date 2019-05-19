6efb45c6c8fdfe5ff5cce2d61d432fe513d3ab82
shlokkh <shlok@shlok.net>
2019-05-18 23:34:05 -0400

    added tests

---
+++ packages/ash/assembly/__tests__/pwd.spec.ts
@@ -0,0 +1,46 @@
+import { Console, fs, Process, CommandLine } from '../../../assemblyscript/assembly/wasa/mock';
+import { FileDescriptor } from '../../../assemblyscript/assembly/wasa/mock/fs';
+import { openStdout, Hello, World } from './mocks';
+import { main as to } from "../bin/pwd";
+
+type fd = usize;
+
+
+
+var stdout2: FileDescriptor;
+// let stdout = Console.stdout;
+
+
+describe("to", (): void => {
+  beforeAll(
+    (): void => {
+      stdout2 = openStdout();
+    }
+  );
+
+  beforeEach((): void => {
+    Console.stdout.reset();
+    stdout2.reset();
+    Console.stdout.erase()
+    CommandLine.reset();
+    CommandLine.push("pwd");
+  })
+
+  it("should give the root path", (): void => {
+
+    // Don't know how to get value of root directory
+    let rootStr = stdout2.readString()
+    exepct<u32>(Console.stdout.readString()).toBe(rootStr)
+
+  })
+
+  it("switch directory", ():void => {
+    //Creates directory test and switches into it
+    CommandLine.push("mkdir test")
+    CommandLine.push("cd test")
+    CommandLine.push("pwd")
+    
+    let testDir = "test"
+    expect<u32>(Console.stdout.readString()).toBe("test")
+  })
+})
