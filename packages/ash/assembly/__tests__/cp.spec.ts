bb4e83d517ba26f3e655af22bbce60f29eba1cff
Michael Williams <mjwilli8@umd.edu>
2019-05-22 02:36:21 -0400

    fixed error in tests. now all tests for cp are passing

--- packages/ash/assembly/__tests__/cp.spec.ts
+++ packages/ash/assembly/__tests__/cp.spec.ts
@@ -34,13 +34,6 @@ describe("cp", (): void => {
     expect<string>(openFile("/newfile").readString().result).toStrictEqual("Hello World");
   });
 
-  it("should copy source file to destination file", (): void => {
-    CommandLine.push("/test");
-    CommandLine.push("/numbers");
-    cp(CommandLine.all());
-    expect<string>(openFile("/numbers").readString().result).toStrictEqual("Hello World");
-  });
-
   it("should copy source file to destination directory", () => {
     CommandLine.push("/test");
     CommandLine.push("/home");
@@ -57,6 +50,13 @@ describe("cp", (): void => {
     expect<bool>(fs.openFile("/home/test").failed).toBeFalsy();
     expect<bool>(fs.openFile("/home/numbers").failed).toBeFalsy();
     expect<string>(openFile("/home/test").readString().result).toStrictEqual("Hello World");
-    expect<string>(openFile("/home/numbers").readString().result).toStrictEqual("0\\n1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n10\\n11\\n12\\n13\\n14\\n15\\n16\\n17\\n18\\n19");
+    expect<string>(openFile("/home/numbers").readString().result).toStrictEqual("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19");
+  });
+
+  it("should copy source file to destination file", (): void => {
+    CommandLine.push("/test");
+    CommandLine.push("/numbers");
+    cp(CommandLine.all());
+    expect<string>(openFile("/numbers").readString().result).toStrictEqual("Hello World");
   });
 })
