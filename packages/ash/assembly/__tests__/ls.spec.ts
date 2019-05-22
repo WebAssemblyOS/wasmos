e5289a371418f44a063ac6d40fc811b1e0e3f851
Frank Lopez <flopez1@terpmail.umd.edu>
2019-05-22 11:09:47 -0400

    Implemented and tested ls. Done.

--- packages/ash/assembly/__tests__/ls.spec.ts
+++ packages/ash/assembly/__tests__/ls.spec.ts
@@ -14,37 +14,19 @@ describe("ls", (): void => {
     });
 
     it("ls test", (): void => {
+        CommandLine.push("home")
         ls(CommandLine.all());
-        log<string>(CommandLine.all().join(" "));
-
-        log<string>(readString(stdout));
         stdout.reset();
+        expect<usize>(Console.stdout.tell()).toBeGreaterThan(0);
+        expect<usize>(Console.stdout.tell()).toBe(22);
         expect<string>(stdout.readString().result).toBe("dev test home numbers\n");
     });
 
-    // it("should print newline by default", (): void => {
-    //     CommandLine.push("/test")
-    //     cat(CommandLine.all())
-    //     let str = Hello_World + "\n";
-    //     expect<u32>(Console.stdout.tell()).toBe(str.lengthUTF8 - 1, "String doesn't have a terminating NUL")
-    //     fs.reset(Console.stdout.fd)
-    //     expect<string>(fs.readString(Console.stdout.fd).result).toBe(Hello + " " + World + "\n")
-    //     Console.stdout.reset()
-    //     expect<string>(Console.stdout.readString().result).toBe(stdout.readString().result);
-    // });
-
-    // it("should write to stderr if file not found", () => {
-    //     CommandLine.push("/doesnotexist");
-    //     cat(CommandLine.all())
-    //     expect<usize>(Console.stderr.tell()).toBeGreaterThan(0);
-    //     expect<string>(stderr.readString().result).toStrictEqual("cat: /doesnotexist: No such file or directory\n")
-    // });
-
-    // it("should write from multiple files", () => {
-    //     CommandLine.push("/test")
-    //     CommandLine.push("/numbers")
-    //     cat(CommandLine.all())
-    //     expect<usize>(Console.stdout.tell()).toBeGreaterThan(Hello_World.lengthUTF8 - 1);
-    // });
-
+    it("ls of a subdirectory test", (): void => {
+        ls(CommandLine.all());
+        stdout.reset();
+        expect<usize>(Console.stdout.tell()).toBeGreaterThan(0);
+        expect<usize>(Console.stdout.tell()).toBe(22);
+        expect<string>(stdout.readString().result).toBe("dev test home numbers\n");
+    });
 })
--- packages/ash/assembly/bin/ls.ts
+++ packages/ash/assembly/bin/ls.ts
@@ -7,14 +7,13 @@ export function main(args: string[]): void {
     } else {
         dirname = fs.getDir(fs.cwd).result.file!.path;
     }
+
     let dir = fs.openDirectory(dirname).result.listDir();
     let _dirs: string[] = new Array<string>();
-    log<usize>(dir.length)
+
     for (let i: i32 = 0; i < dir.length; i++) {
         _dirs.push(dir[i].path);
-        log<string>(dir[i].path);
     }
-    log<usize>(_dirs.join(" ").length);
-    log<usize>(Console.stdout.offset)
+
     Console.log(_dirs.join(" "));
 }
