29908f71163f4d9ddfa3efa7d102ca167e616f4f
Willem Wyndham <willem.neal@gmail.com>
2019-05-23 14:52:13 -0400

    Added real path to path

--- packages/ash/assembly/__tests__/basename.spec.ts
+++ packages/ash/assembly/__tests__/basename.spec.ts
@@ -3,24 +3,23 @@ import { main as echo } from "../bin/basename";
 
 describe("basename", (): void => {
 
-  beforeEach((): void => {
-      stdout.reset();
-          Console.stdout.erase();
-	      CommandLine.reset();
-	          CommandLine.push("basename");
-		    })
+	beforeEach((): void => {
+		stdout.reset();
+		Console.stdout.erase();
+		CommandLine.reset();
+		CommandLine.push("basename");
+	})
 
-		      it("should print newline by default", (): void => {
-		          CommandLine.push(Hello)
-			      CommandLine.push(World)
-			          echo(CommandLine.all())
-				      let str = Hello + " " + World + "\n";
-				          let stdoutStr = readString(stdout)
-					      expect<u32>(Console.stdout.tell()).toBe(str.lengthUTF8 - 1, "No NUL character at the end of the string")
-						          Console.stdout.reset();
-							      expect<string>(readString(Console.stdout)).toBe(Hello + " " + World + "\n")
-								          Console.stdout.reset();
-									      expect<string>(readString(Console.stdout)).toBe(stdoutStr);
-										        });
+	it("should return the last part of the path", (): void => {
+		CommandLine.push("/home/bob/documents/secret.txt"); //
+		echo(CommandLine.all())
+		let str = "\n"; //Fixme
+		let stdoutStr = readString(stdout)
+		expect<u32>(Console.stdout.tell()).toBe(str.lengthUTF8 - 1, "No NUL character at the end of the string")
+		Console.stdout.reset();
+		expect<string>(readString(Console.stdout)).toBe(str)
+		Console.stdout.reset();
+		expect<string>(readString(Console.stdout)).toBe(stdoutStr);
+	});
 
-											})
+})
--- packages/ash/assembly/bin/basename.ts
+++ packages/ash/assembly/bin/basename.ts
@@ -1,8 +1,9 @@
+import * as path from "../../../assemblyscript/assembly/wasa/mock/path";
+
 export function main(args: string[]): void {
 	if (args.length > 1) {
 		let dirname = args[1];
-		var path = require('path');
 		var filename = path.basename(dirname);
-		console.log(filename);
+		Console.log(filename);
 	}
 }
